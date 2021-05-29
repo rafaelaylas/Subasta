e8d12bcb62d9de4551db1fc7df86d65c054a3174
rafaelaylas <rafael.aylas@gmail.com>
2021-05-29 16:38:31 -0300

    Segunda prueba de commit.

---
+++ app/src/main/java/com/platzi/subasta/CallbackFragment.java
@@ -0,0 +1,5 @@
+package com.platzi.subasta;
+
+public interface CallbackFragment {
+    void changeFragment();
+}
--- app/src/main/java/com/platzi/subasta/MainActivity.java
+++ app/src/main/java/com/platzi/subasta/MainActivity.java
@@ -1,13 +1,27 @@
 package com.platzi.subasta;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.platzi.subasta.ui.account.AccountFragment;
+import com.platzi.subasta.ui.article.ArticleFragment;
+import com.platzi.subasta.ui.bid.BidFragment;
+import com.platzi.subasta.ui.home.HomeFragment;
+import com.platzi.subasta.ui.payment.PaymentFragment;
+import com.platzi.subasta.ui.statistic.EstatisticFragment;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,7 +31,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+/*
+    Fragment fragment;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+*/
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
@@ -34,7 +52,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        // final para que sea accesible desde adentro
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -46,6 +66,44 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int opcionSeleccionada = item.getItemId();
+
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                Fragment fragment = null;
+
+                drawer.closeDrawer(GravityCompat.START);
+                switch(opcionSeleccionada)
+                {
+                    case R.id.nav_home:
+                        fragment = new LoginFragment();
+                        break;
+                    case R.id.nav_account:
+                        fragment = new AccountFragment();
+                        break;
+                    case R.id.nav_payment:
+                        fragment = new PaymentFragment();
+                        break;
+                    case R.id.nav_article:
+                        fragment = new ArticleFragment();
+                        break;
+                    case R.id.nav_bid:
+                        fragment = new BidFragment();
+                        break;
+                    case R.id.nav_statistic:
+                        fragment = new EstatisticFragment();
+                        break;
+                    default:
+                }
+                fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
+                fragmentTransaction.commit();
+                return false;
+            }
+        });
     }
 
     @Override
@@ -56,9 +114,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int idPulsado = item.getItemId();
+        if(idPulsado == R.id.action_settings)
+            Toast.makeText(MainActivity.this, "Hola desde el menu aplicacion", Toast.LENGTH_SHORT).show();;
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+/*
+    public void addFragment(){
+        LoginFragment fragment = new LoginFragment();
+        fragment.setCallbackFragment(this);
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        //Hace referencia al ActivityMain
+        //FragmentTransaction.add(R.id.nav_host_fragment, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void changeFragment() {
+        replaceFragment();
+    }
+
+    private void replaceFragment() {
+        RegisterFragment fragment = new RegisterFragment();
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
+        fragmentTransaction.commit();
+    }
+
+ */
 }
